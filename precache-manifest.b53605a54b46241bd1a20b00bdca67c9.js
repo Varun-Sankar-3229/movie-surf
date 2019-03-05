4657ed40dbbdbc8a6f179beec5d879e283434a28
unknown <varunitssankar@gmail.com>
2019-03-05 13:46:36 +0530

    Updates

--- precache-manifest.b53605a54b46241bd1a20b00bdca67c9.js
+++ precache-manifest.b53605a54b46241bd1a20b00bdca67c9.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/movie-surf/static/js/runtime~main.9464e054.js"
   },
   {
-    "revision": "499e6c5b1baf6dc2ba33",
-    "url": "/movie-surf/static/js/main.499e6c5b.chunk.js"
+    "revision": "5906f9de59db96211700",
+    "url": "/movie-surf/static/js/main.5906f9de.chunk.js"
   },
   {
     "revision": "13548fab87af0e09b469",
     "url": "/movie-surf/static/js/1.13548fab.chunk.js"
   },
   {
-    "revision": "499e6c5b1baf6dc2ba33",
+    "revision": "5906f9de59db96211700",
     "url": "/movie-surf/static/css/main.f39ae108.chunk.css"
   },
   {
-    "revision": "f092821cf289408c224fc6823b9b325f",
+    "revision": "46ea4423addd0ccda1513bf9c15e5cb0",
     "url": "/movie-surf/index.html"
   }
 ];
