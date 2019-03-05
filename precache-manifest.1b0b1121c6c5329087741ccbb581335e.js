e93822f71a9ba7a862f3cba965959c74e726a04f
unknown <varunitssankar@gmail.com>
2019-03-05 13:50:17 +0530

    Updates

--- precache-manifest.1b0b1121c6c5329087741ccbb581335e.js
+++ precache-manifest.1b0b1121c6c5329087741ccbb581335e.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/movie-surf/static/js/runtime~main.9464e054.js"
   },
   {
-    "revision": "5906f9de59db96211700",
-    "url": "/movie-surf/static/js/main.5906f9de.chunk.js"
+    "revision": "c59b0edaf3799f0bf8ee",
+    "url": "/movie-surf/static/js/main.c59b0eda.chunk.js"
   },
   {
     "revision": "13548fab87af0e09b469",
     "url": "/movie-surf/static/js/1.13548fab.chunk.js"
   },
   {
-    "revision": "5906f9de59db96211700",
+    "revision": "c59b0edaf3799f0bf8ee",
     "url": "/movie-surf/static/css/main.f39ae108.chunk.css"
   },
   {
-    "revision": "46ea4423addd0ccda1513bf9c15e5cb0",
+    "revision": "c49a228819eff054d14be4e376aa51fa",
     "url": "/movie-surf/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/movie-surf/precache-manifest.b53605a54b46241bd1a20b00bdca67c9.js"
+  "/movie-surf/precache-manifest.1b0b1121c6c5329087741ccbb581335e.js"
 );
 
 workbox.clientsClaim();
